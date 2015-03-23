a4e0db5ed5e189da762dd890edb42e8c2bbdb21e
iga <a@a.com>
2015-03-23 23:46:15 +0100

    Finished

--- app/src/main/java/com/innblue/modernartui/MainActivity.java
+++ app/src/main/java/com/innblue/modernartui/MainActivity.java
@@ -1,24 +1,32 @@
 package com.innblue.modernartui;
 
+import android.app.FragmentManager;
 import android.graphics.Color;
-import android.graphics.drawable.shapes.Shape;
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBarActivity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    //initial value for RED and GREEN for the tile , BLUE are driven by progress in onProgressChanged
+    private final static Map<Integer, Integer> rectangleToStartingColorMapping = Collections.unmodifiableMap(new HashMap<Integer, Integer>() {{
+        put(R.id.rectimage2, 0);
+        put(R.id.rectimage3, 60);
+        put(R.id.rectimage4, 120);
+        put(R.id.rectimage5, 180);
+    }});
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,15 +49,10 @@ public class MainActivity extends ActionBarActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+        FragmentManager fm = getFragmentManager();
+        MoMaDialogFragment moMaDialogFragment = new MoMaDialogFragment();
+        moMaDialogFragment.setRetainInstance(true);
+        moMaDialogFragment.show(fm, "fragment_moma_dialog");
 
         return super.onOptionsItemSelected(item);
     }
@@ -65,30 +68,39 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-            final ImageView rect = (ImageView) rootView.findViewById(R.id.rectimage1);
+            final View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            //set initial colors
+            setColors(rootView, 0);
 
             SeekBar seekBar = (SeekBar) rootView.findViewById(R.id.seekBar);
             seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
                 @Override
                 public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                    //rect.setBackgroundColor(r);
+                    //for each rectangle (but white, which is not in the mapping) change color of  BLUE components to "progress" value
+                    setColors(rootView, progress);
                 }
 
                 @Override
                 public void onStartTrackingTouch(SeekBar seekBar) {
-
                 }
 
                 @Override
                 public void onStopTrackingTouch(SeekBar seekBar) {
-
                 }
             });
 
             return rootView;
         }
 
-
+        //set colors (called initially and the on each progress change)
+        private void setColors(View rootView, int progress) {
+            for (Map.Entry<Integer, Integer> entry : rectangleToStartingColorMapping.entrySet()) {
+                //find rectangle
+                ImageView rectangle = (ImageView) rootView.findViewById(entry.getKey());
+                //change color
+                rectangle.setBackgroundColor(Color.rgb(entry.getValue(), entry.getValue(), progress));
+            }
+        }
     }
+
 }
